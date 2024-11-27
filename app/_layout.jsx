54534ca40731ffd5ff2fc0df0397d96781a117c4
PrinceH00 <hamz0230@stud.kea.dk>
2024-11-27 13:26:07 +0100

    Added signOut button on department menu

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,22 @@
-import { Stack } from "expo-router";
+import {router, Stack} from "expo-router";
+import {signingOut} from "@/app/signOut";
+import {Button} from "react-native";
 
 export default function RootLayout() {
     return (
         <Stack>
-            <Stack.Screen name="index" options={{headerShown: false}} />
+            <Stack.Screen name="index" options={{headerShown: false}}/>
             <Stack.Screen name={'signIn'} options={{headerShown: false}}/>
+
+            <Stack.Screen name={'department'} options={{
+                headerLeft: null, headerRight: () => (
+                    <Button
+                        onPress={() => signingOut(router)}
+                        title="Sign Out"
+                        color="black"
+                    />
+                )
+            }}/>
         </Stack>
     );
 }
