b7086931ddb745736a628364f59ae72fb6ec0b88
PrinceH00 <hamz0230@stud.kea.dk>
2024-12-17 00:20:25 +0100

    delete sign in

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,24 +3,22 @@ import { signingOut } from "@/app/signOut";
 import { Button } from "react-native";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name={"signIn"} options={{ headerShown: false }} />
-
-      <Stack.Screen
-        name={"department"}
-        options={{
-          headerLeft: null,
-          headerRight: () => (
-            <Button
-              onPress={() => signingOut(router)}
-              title="Sign Out"
-              color="black"
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen
+                name={"department"}
+                options={{
+                    headerLeft: null,
+                    headerRight: () => (
+                        <Button
+                            onPress={() => signingOut(router)}
+                            title="Sign Out"
+                            color="black"
+                        />
+                    ),
+                }}
             />
-          ),
-        }}
-      />
-    </Stack>
-  );
+        </Stack>
+    );
 }
