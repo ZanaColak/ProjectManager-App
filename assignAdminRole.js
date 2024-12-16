389839103ff9448e59575bf2fac054e0f604b7b5
PrinceH00 <hamz0230@stud.kea.dk>
2024-12-17 00:17:34 +0100

    Assign Admin fucktion with firebase Admin SDK

---
+++ assignAdminRole.js
@@ -0,0 +1,25 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+const assignAdminRole = async (uid) => {
+    try {
+        const userRef = db.collection("users").doc(uid);
+        const userDoc = await userRef.get();
+
+        if (!userDoc.exists) {
+            await userRef.set({ role: "admin" });
+        } else {
+            await userRef.update({ role: "admin" });
+        }
+    } catch (error) {
+        console.error("Error updating role:", error);
+    }
+};
+
+assignAdminRole("P5sLltcuudRgqaqR3XcGlTqWsWr1");
