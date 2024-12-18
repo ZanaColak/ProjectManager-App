10e0326f6e135b5310dac915885835961c424a87
PrinceH00 <hamz0230@stud.kea.dk>
2024-12-18 19:34:15 +0100

    Add new screens to Stack navigation in RootLayout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,28 +1,49 @@
-import { Stack } from "expo-router";
-import { signingOut } from "app/components/signOut";
-import { useRouter } from "expo-router";
-import { Button } from "react-native";
+import { useRouter,Stack } from "expo-router";
+import {signingOut} from "./components/signOut";
+import {Button} from "react-native";
 
 export default function RootLayout() {
     const router = useRouter();
 
     return (
         <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen name="dashboard" options={{headerRight: () => (
-                        <Button
-                            onPress={() => signingOut(router)} // Pass router correctly
+            {/* Index Screen */}
+            <Stack.Screen name="index" options={{headerShown: false}}/>
+
+            {/* Department Screen */}
+            <Stack.Screen name="department" options={{
+                headerRight: () => (
+                    <Button onPress={() => signingOut(router)}
                             title="Sign Out"
                             color="black"
-                        />
-                ),}}/>
-            <Stack.Screen name="department" options={{headerRight: () => (
-                        <Button
-                            onPress={() => signingOut(router)} // Pass router correctly
+                    />
+                ),
+            }}
+            />
+
+            {/* Dashboard Screen */}
+            <Stack.Screen name="dashboard" options={{
+                headerRight: () => (
+                    <Button onPress={() => signingOut(router)}
                             title="Sign Out"
                             color="black"
-                        />
-                ),}}/>
+                    />
+                ),
+            }}
+            />
+
+            {/* Project Screen */}
+            <Stack.Screen name="project" options={{headerShown: true}}/>
+
+            {/* Scrum board Screen */}
+            <Stack.Screen name="scrumboard" options={{headerShown: true}}/>
+
+            {/* Calender Screen */}
+            <Stack.Screen name="calendar" options={{headerShown: true}}/>
+
+            {/* Timeline Screen */}
+            <Stack.Screen name="timeline" options={{headerShown: true}}/>
+
         </Stack>
     );
 }
