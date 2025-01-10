25d5614b2bb5099570340278c27191220621deee
PrinceH00 <hamz0230@stud.kea.dk>
2025-01-10 07:48:02 +0100

    Refactored and Added storage clear for web and IOS

--- app/components/signOut.jsx
+++ app/components/signOut.jsx
@@ -1,18 +1,26 @@
 import { signOut } from "firebase/auth";
 import { auth } from "../config/firebase";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {Platform} from "react-native";
 
 export const signingOut = async (router) => {
     try {
         await signOut(auth);
 
+        if (Platform.OS === "web") {
+            localStorage.clear();
+        } else {
+            await AsyncStorage.clear();
+        }
+
         if (router) {
-            router.push("/");
+            router.replace("/");
 
         } else {
             console.warn("Router is not defined. Unable to redirect after sign-out.");
         }
     } catch (error) {
         console.error("Sign out error:", error);
-        alert("Failed to sign out. Please try again.");
+        alert("Fejl, " + "Kunne ikke logge ud. Prøv venligst igen.");
     }
 };
